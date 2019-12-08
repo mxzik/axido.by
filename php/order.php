97c1758a1f291ad8e3953917527bfe758253b204
mxzik <54948529+mxzik@users.noreply.github.com>
2019-12-08 16:13:24 +0300

    Add files via upload

---
+++ php/order.php
@@ -0,0 +1,33 @@
+<?php
+$lastname = $_POST['lastname'];
+$firstname = $_POST['firstname'];
+$email = $_POST['email'];
+$country = $_POST['email'];
+$comment = $_POST['comment'];
+$confirm = $_POST['confirm'];
+$lastname = htmlspecialchars($lastname);
+$firstname = htmlspecialchars($firstname);
+$email = htmlspecialchars($email);
+$country = htmlspecialchars($country);
+$comment = htmlspecialchars($comment);
+$confirm = htmlspecialchars($confirm);
+$lastname = urldecode($lastname);
+$firstname = urldecode($firstname);
+$email = urldecode($email);
+$country = urldecode($country);
+$comment = urldecode($comment);
+$confirm = urldecode($confirm);
+$lastname = trim($lastname);
+$firstname = trim($firstname);
+$email = trim($email);
+$country = trim($country);
+$comment = trim($comment);
+$confirm = trim($confirm);
+echo $lastname;
+echo "<br>";
+echo $email;
+if (mail("puderasts@mail.ru", "Отзыв", "Фамилия:".$lastname.".Имя: ".$firstname. ". E-mail: ".$email. ".Страна: ".$country. ".Отзыв: ".$comment. ".Подтверждение на использование: ".$confirm , "From: ximachk@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
